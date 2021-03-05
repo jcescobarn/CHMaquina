0ffd9eea6f8e831ed447d0f03ef833d9f597f147
jcescobarn <jcescobarn@unal.edu.co>
2021-03-04 22:00:01 -0500

    dependencies installation

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from '../../logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  render(){
+    return <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,8 +19,8 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
+    </div>;
+  }
 }
 
 export default App;
